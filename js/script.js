48f188b102e8c57cf4364fc7f33b3a2ddf5186c2
jodiduckert <jodiduckert@yahoo.com>
2016-01-27 22:37:11 -0600

    hover links only on desktop

--- js/script.js
+++ js/script.js
@@ -1,42 +1,46 @@
 $(document).ready(function() {
 
-	$('#about').mouseenter(function() {
-		$('#about > a').css('color', '#82c0c0'),
-		$('#about').css('background-color', 'white');
-	});
+	$(window).resize(function(){
+       if ($(window).width() <= 750) {  
+		    $('#about').mouseenter(function() {
+			$('#about > a').css('color', '#82c0c0'),
+			$('#about').css('background-color', 'white');
+			});
 
-	$('#academics').mouseenter(function() {
-		$('#academics > a').css('color', '#82c0c0'),
-		$('#academics').css('background-color', 'white');
-	});
+			$('#academics').mouseenter(function() {
+				$('#academics > a').css('color', '#82c0c0'),
+				$('#academics').css('background-color', 'white');
+			});
 
-	$('#sites').mouseenter(function() {
-		$('#sites > a').css('color', '#82c0c0'),
-		$('#sites').css('background-color', 'white');
-	});
+			$('#sites').mouseenter(function() {
+				$('#sites > a').css('color', '#82c0c0'),
+				$('#sites').css('background-color', 'white');
+			});
 
-	$('#costs').mouseenter(function() {
-		$('#costs > a').css('color', '#82c0c0'),
-		$('#costs').css('background-color', 'white');
-	});
+			$('#costs').mouseenter(function() {
+				$('#costs > a').css('color', '#82c0c0'),
+				$('#costs').css('background-color', 'white');
+			});
 
-	$('#about').mouseleave(function() {
-		$('#about > a').css('color', 'white'),
-		$('#about').css('background-color', '#82c0c0');
-	});
+			$('#about').mouseleave(function() {
+				$('#about > a').css('color', 'white'),
+				$('#about').css('background-color', '#82c0c0');
+			});
 
-	$('#academics').mouseleave(function() {
-		$('#academics > a').css('color', 'white'),
-		$('#academics').css('background-color', '#82c0c0');
-	});
+			$('#academics').mouseleave(function() {
+				$('#academics > a').css('color', 'white'),
+				$('#academics').css('background-color', '#82c0c0');
+			});
 
-	$('#sites').mouseleave(function() {
-		$('#sites > a').css('color', 'white'),
-		$('#sites').css('background-color', '#82c0c0');
-	});
+			$('#sites').mouseleave(function() {
+				$('#sites > a').css('color', 'white'),
+				$('#sites').css('background-color', '#82c0c0');
+			});
 
-	$('#costs').mouseleave(function() {
-		$('#costs > a').css('color', 'white'),
-		$('#costs').css('background-color', '#82c0c0');
+			$('#costs').mouseleave(function() {
+				$('#costs > a').css('color', 'white'),
+				$('#costs').css('background-color', '#82c0c0');
+			});
+       }     
 	});
 });
