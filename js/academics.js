fb82cfee23147998bda29182f18c68fa51ec0ce7
Lwalz <lukewalz@creighton.edu>
2016-02-10 21:57:28 -0600

    animated arrow

---
+++ js/academics.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('#s').click(function(){
+		$('#sArrow').css('transform','rotate(90deg)');
+		$('#cArrow').css('transform','rotate(270deg)');
+});
+	$('#c').click(function(){
+		$('#sArrow').css('transform','rotate(360deg)');
+		$('#cArrow').css('transform','rotate(360deg)');
+});
+});
