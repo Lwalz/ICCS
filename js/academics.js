a86ab4ca7b037c7ce2cf5286e5e01e4d5318ef5b
Lwalz <lcw24457@as-jmc205dins.creighton.edu>
2016-03-02 13:43:57 -0600

    nav

--- js/academics.js
+++ js/academics.js
@@ -1,15 +1,13 @@
 $(document).ready(function(){
-	$('#s').click(function(){
-		$('#sArrow').css('transform','rotate(90deg)');
-		$('#cArrow').css('transform','rotate(270deg)');
-});
-	$('#c').click(function(){
-		$('#sArrow').css('transform','rotate(360deg)');
-		$('#cArrow').css('transform','rotate(360deg)');
-});
 
 	$('#academics').css('background-color','white');
 	$('#academics').css('color','blue !important');
+	
+  // Bind the swipeHandler callback function to the swipe event on div.box
+  $('#main').click(function(){
+	  $('#more').css('display','inline-block');
+	  });
+ 
 
 
 	
