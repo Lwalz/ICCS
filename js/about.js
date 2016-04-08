cf44fab579eb5b6ee5c078ffcb7f0fd03781c464
ballstodawalz <lukewalz@creighton.edu>
2016-04-07 23:02:53 -0500

    jhg

--- js/about.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-	$('#main').click(function(){
-		$('#more').toggle('slow')
-	})
-})
--- js/academics.js
+++ js/academics.js
@@ -1,26 +1,14 @@
 $(document).ready(function(){
-	
-	$('#c').click(function(){
-		$('#coursebar').toggle('slow');
-	});
-	
-	$('#s').click(function(){
-		$('#scholarshipbar').toggle('slow');
-	});
 
 	$('#academics').css('background-color','white');
 	$('#academics').css('color','blue !important');
 	
   // Bind the swipeHandler callback function to the swipe event on div.box
   $('#main').click(function(){
-	  $('#more').toggle('slow');
+	  $('#more').css('display','inline-block');
 	  });
-	   
-	   $('#columnSite1')(function(){
-		   $('#carousel1').carousel('pause');
-	   });
-	   
-	   $('#columnSite1').hover()(function(){
-		   $('#carousel1').carousel('cycle');
-	   });
+ 
+
+
+	
 });
