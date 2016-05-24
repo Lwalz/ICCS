33b34c27758388d39acf7afdba10d5852d162bf5
Lwalz <lukewalz@creighton.edu>
2016-05-24 02:30:14 -0500

    fhgfhgfkjfhj

--- js/about.js
+++ js/about.js
@@ -2,4 +2,34 @@ $(document).ready(function(){
 	$('#main').click(function(){
 		$('#more').toggle('slow')
 	})
+
+	$('#readMoreButton').click(function(){
+		$('#readMoreButton').css('display','none');
+		$('#readMore').css('display','inline-block')
+	})
+
+	$('#one').click(function(){
+		$('#oneTxt').toggle('slow');
+	})
+
+	$('#two').click(function(){
+		$('#twoTxt').toggle('slow');
+	})
+
+	$('#three').click(function(){
+		$('#threeTxt').toggle('slow');
+	})
+
+	$('#four').click(function(){
+		$('#fourTxt').toggle('slow');
+	})
+
+	$('#five').click(function(){
+		$('#fiveTxt').toggle('slow');
+	})
+
+	$('#six').click(function(){
+		$('#sixTxt').toggle('slow');
+	})
+
 })
