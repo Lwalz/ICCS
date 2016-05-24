b426b7bb68bfaca58cb04a39f8ea06f7c7c835ea
Lwalz <lukewalz@creighton.edu>
2016-05-24 18:33:42 -0500

    jglhj

--- js/academics.js
+++ js/academics.js
@@ -1,26 +1,23 @@
-$(document).ready(function(){
-	
-	$('#c').click(function(){
+$(document).ready(function() {
+  $('#c').click(function(){
 		$('#coursebar').toggle('slow');
-	});
-	
-	$('#s').click(function(){
-		$('#wsabar').toggle('slow');
-	});
+});
+
+	$('#s').click(function() {
+  $('#wsabar').toggle('slow');
+});
 
 	$('#academics').css('background-color','white');
 	$('#academics').css('color','blue !important');
-	
+
   // Bind the swipeHandler callback function to the swipe event on div.box
-  $('#main').click(function(){
-	  $('#more').toggle('slow');
-	  });
-	   
-	   $('#columnSite1')(function(){
-		   $('#carousel1').carousel('pause');
-	   });
-	   
-	   $('#columnSite1').hover()(function(){
-		   $('#carousel1').carousel('cycle');
-	   });
+  $('#main').click(function() {
+  $('#more').toggle('slow');
+});
+
+$('#carousel1').carousel( {
+  interval: 50000;
+});
+
+
 });
