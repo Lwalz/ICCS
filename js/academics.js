743deab3f6e7fa0743fdbc2dd00198be906f9b79
Lwalz <lukewalz@creighton.edu>
2016-05-25 13:05:19 -0500

    kjhgkfd

--- js/academics.js
+++ js/academics.js
@@ -15,9 +15,43 @@ $(document).ready(function(){
   $('#main').click(function(){
 	  $('#more').toggle('slow');
   })
+var c1 = $('#carousel1');
+var c2 = $('#carousel2');
+var c3 = $('#carousel3');
 
-  $('#carousel1').carousel();
+var c = [c1,c2,c3];
+
+function runMe(i){
+  i.carousel({interval:'2000'})
+}
+var count = 0
+runMe(c1)
+
+c1.on('slid.bs.carousel', function() {
+  count=count+1
+  if (count==3) {
+    c1.carousel('pause')
+    runMe(c2)
+    count=0
+  }
+
+})
+c2.on('slid.bs.carousel', function() {
+  count=count+1
+  if (count==3) {
+    c2.carousel('pause')
+    runMe(c3)
+    count=0
+  }
+})
+c3.on('slid.bs.carousel', function() {
+  count=count+1
+  if (count==3) {
+    c3.carousel('pause')
+    runMe(c1)
+    count=0
+  }
+})
 
 
-
-});
+})
