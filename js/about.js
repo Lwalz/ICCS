449ddbc7be784e7133c48bc9c26fabbdba7e32f9
Lwalz <lukewalz@creighton.edu>
2016-05-26 21:53:49 -0500

    ,ghcghj

--- js/about.js
+++ js/about.js
@@ -1,40 +1,48 @@
-$(document).ready(function() {
-  $('#main').click(function(){
+$(document).ready(function(){
+	$('#main').click(function(){
 		$('#more').toggle('slow')
-})
+	})
 
-	$('#readMoreButton').click(function() {
-  $('#readMoreButton').css('display','none');
+	$('#readMoreButton').click(function(){
+		$('#readMoreButton').css('display','none');
 		$('#readMore').css('display','inline-block')
-})
+	})
 
-	$('#one').click(function() {
-  $('#oneTxt').toggle('slow');
-		$(this).siblings().toggle('slow');
-})
+	$('#one').click(function(){
+		$('#oneTxt').toggle('slow');
+		$(this).siblings().toggle();
 
-	$('#two').click(function() {
-  $('#twoTxt').toggle('slow');
-		$(this).siblings().toggle('slow');
-})
+	})
 
-	$('#three').click(function() {
-  $('#threeTxt').toggle('slow');
-		$(this).siblings().toggle('slow');
-})
+	$('#two').click(function(){
+		$('#twoTxt').toggle('slow');
+		$(this).siblings().toggle();
 
-	$('#four').click(function() {
-  $('#fourTxt').toggle('slow');
-		$(this).siblings().toggle('slow');
-})
+	})
 
-	$('#five').click(function() {
-  $('#fiveTxt').toggle('slow');
-		$(this).siblings().toggle('slow');
-})
+	$('#three').click(function(){
+		$('#threeTxt').toggle('slow');
+		$(this).siblings().toggle();
 
-	$('#six').click(function() {
-  $('#sixTxt').toggle('slow');
-		$(this).siblings().toggle('slow');
-})
+	})
+
+	$('#four').click(function(){
+		$('#fourTxt').toggle('slow');
+		$(this).siblings().toggle();
+
+	})
+
+	$('#five').click(function(){
+		$('#fiveTxt').toggle('slow');
+		$(this).siblings().toggle();
+
+	})
+
+	$('#six').click(function(){
+		$('#sixTxt').toggle('slow');
+		$(this).siblings().toggle();
+
+	})
+	  $('[data-toggle="popover"]').popover()
+
 })
