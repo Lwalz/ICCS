bd564a2db4d1f09a669fb0653e5533c4212dade9
Lwalz <lukewalz@creighton.edu>
2016-06-26 20:26:36 -0500

    lkjdkljfg'dg

---
+++ footer.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="stylesheet" href="css/styleFoot.css">
+</head>
+<body>
+	    <section id="foot" class="footer">
+        <div id="uno">Susan E. Pratt, Assistant Director/GEO Program Manager<br>          Tel. (919) 684-2174<br>
+
+        </div>
+        <a href="mailto:pratt.susan@duke.edu" id="email">pratt.susan@duke.edu</a>
+        <a href="https://www.facebook.com/groups/212322858795713/" id="fb"><img src="images/fb.png"></a>
+
+    </section>
+</body>
+</html>
